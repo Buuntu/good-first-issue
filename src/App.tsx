35aeb604282defac1856e793f4a1c00c2fd4d6d5
Gabe Abud <gabriel.jabud@gmail.com>
2020-07-09 10:14:27 -0700

    add default sorting, add labels and date column, add links

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import {
   Button,
   CircularProgress,
   Typography,
+  Chip,
 } from '@material-ui/core';
 import { useForm } from 'react-hook-form';
 import MaterialTable from 'material-table';
@@ -48,12 +49,21 @@ type GithubResponseType = {
   incomplete_result: boolean;
 };
 
+type GithubLabelType = {
+  name: string;
+  description: string;
+};
+
 type GithubIssueType = {
   url: string;
   repository_url: string;
   html_url: string;
   title: string;
-  labels: string;
+  labels: GithubLabelType[];
+  assignee: string | null;
+  comments: string[];
+  body: string;
+  created_at: string;
 };
 
 const App = () => {
@@ -62,12 +72,21 @@ const App = () => {
   const [results, setResults] = useState<GithubIssueType[]>([]);
   const [isFetching, setIsFetching] = useState<boolean>(false);
 
+  const getRepoFromUrl = (url: string) => {
+    const regex = /\/repos\/(([^/]+)\/([^/]+))/;
+    const found = url.match(regex);
+
+    return found ? found[1] : url;
+  };
+
+  const buildSearchString = (query: string): string => {
+    return `https://api.github.com/search/issues?q=${query}+label:"good first issue"+is:"open"`;
+  };
+
   const searchGithub = async (searchString: string) => {
     setIsFetching(true);
     try {
-      const data = await fetch(
-        `https://api.github.com/search/issues?q=${searchString}+label:"good first issue"+is:"open"`,
-      );
+      const data = await fetch(buildSearchString(searchString));
 
       if (data.ok) {
         const json = await data.json();
@@ -75,6 +94,7 @@ const App = () => {
         setResults(json.items);
       }
     } catch (err) {
+      alert('There was an error on the server');
     } finally {
       setIsFetching(false);
     }
@@ -90,7 +110,7 @@ const App = () => {
             alignItems="center"
             direction="column"
           >
-            <Grid item xs={12} alignContent="center">
+            <Grid item xs={12}>
               <Typography
                 className={classes.header}
                 variant="h2"
@@ -99,7 +119,7 @@ const App = () => {
                 good first issue
               </Typography>
             </Grid>
-            <Grid item xs={12} alignContent="center">
+            <Grid item xs={12}>
               <GitHubButton
                 href="https://github.com/Buuntu/good-first-issue"
                 data-color-scheme="no-preference: light; light: light; dark: dark;"
@@ -111,71 +131,104 @@ const App = () => {
               </GitHubButton>
             </Grid>
           </Grid>
-          <Paper className={classes.card}>
-            <form
-              noValidate
-              onSubmit={handleSubmit((data) => searchGithub(data.search))}
-            >
-              <Grid container>
-                <Grid item xs={12}>
-                  <TextField
-                    inputRef={register}
-                    required
-                    name="search"
-                    variant="outlined"
-                    size="small"
-                    label="Search GitHub"
-                  ></TextField>
-                  <Button
-                    type="submit"
-                    variant="outlined"
-                    className={classes.button}
-                  >
-                    Submit
-                  </Button>
+          <Grid item xs={12} md={8} lg={6}>
+            <Paper className={classes.card}>
+              <form
+                noValidate
+                onSubmit={handleSubmit((data) => searchGithub(data.search))}
+              >
+                <Grid container>
+                  <Grid item xs={12}>
+                    <TextField
+                      inputRef={register}
+                      required
+                      name="search"
+                      variant="outlined"
+                      size="small"
+                      label="Search GitHub"
+                    ></TextField>
+                    <Button
+                      type="submit"
+                      variant="outlined"
+                      className={classes.button}
+                    >
+                      Submit
+                    </Button>
+                  </Grid>
                 </Grid>
+              </form>
+              <Grid container>
+                {isFetching ? (
+                  <CircularProgress />
+                ) : (
+                  <>
+                    {results.length > 0 ? (
+                      <Grid item xs={12}>
+                        <MaterialTable
+                          title={''}
+                          options={{
+                            padding: 'dense',
+                            rowStyle: {
+                              fontSize: '14px',
+                            },
+                          }}
+                          icons={tableIcons}
+                          columns={[
+                            {
+                              title: 'Title',
+                              field: 'title',
+                              render: (rowData) => (
+                                <a href={rowData.html_url} target="_blank">
+                                  {rowData.title}
+                                </a>
+                              ),
+                            },
+
+                            {
+                              title: 'Labels',
+                              field: 'labels',
+                              render: (rowData) =>
+                                rowData.labels.map((label) => (
+                                  <Chip label={label.name} />
+                                )),
+                            },
+                            {
+                              title: 'Repo',
+                              field: 'repository_url',
+                              render: (rowData) => {
+                                const repo = getRepoFromUrl(
+                                  rowData.repository_url,
+                                );
+
+                                return (
+                                  <a
+                                    href={`https://github.com/${repo}`}
+                                    target="_blank"
+                                  >
+                                    {repo}
+                                  </a>
+                                );
+                              },
+                            },
+                            {
+                              title: 'Date Created',
+                              field: 'created_at',
+                              defaultSort: 'desc',
+                              render: (rowData) =>
+                                new Date(rowData.created_at).toDateString(),
+                            },
+                          ]}
+                          data={results}
+                        />
+                      </Grid>
+                    ) : (
+                      'No Results'
+                    )}
+                  </>
+                )}
               </Grid>
-            </form>
-            <Grid container>
-              {isFetching ? (
-                <CircularProgress />
-              ) : (
-                <>
-                  {results.length > 0 ? (
-                    <Grid item xs={12}>
-                      <MaterialTable
-                        title={''}
-                        options={{
-                          padding: 'dense',
-                          rowStyle: {
-                            fontSize: '14px',
-                          },
-                        }}
-                        icons={tableIcons}
-                        columns={[
-                          {
-                            title: 'Title',
-                            field: 'title',
-                          },
-                          {
-                            title: 'URL',
-                            field: 'html_url',
-                          },
-                          {
-                            title: 'Repository Url',
-                            field: 'repository_url',
-                          },
-                        ]}
-                        data={results}
-                      />
-                    </Grid>
-                  ) : (
-                    'No Results'
-                  )}
-                </>
-              )}
-            </Grid>
-          </Paper>
+            </Paper>
+          </Grid>
         </Grid>
       </header>
     </div>
